f1396cbc9817ce65e906c64fbf4c46a8321a193f
giosefiele <giuseppe.ferlazzo.101@gmail.com>
2026-05-27 17:52:16 +0200

    NON HO IL VACCINO

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,6 +3,8 @@ import Script from "next/script";
 import React from "react";
 
 const Contact = () => {
+  const [io, setIo] = React.useState(false);
+  setIo(true);
   return (
     <div>
       <Script
@@ -11,6 +13,16 @@ const Contact = () => {
         crossOrigin="anonymous"
       />
       <Contactoo />
+      {io && (
+        <ins
+          className="adsbygoogle"
+          style={{ display: "block" }}
+          data-ad-client="ca-pub-7088214308316238"
+          data-ad-slot="1234567890"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
+      )}
     </div>
   );
 };
